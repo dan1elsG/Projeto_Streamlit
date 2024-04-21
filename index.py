db9343aa3e89d506035e2f1533afdbbd5a8e337d
dan1elsG <157855081+dan1elsG@users.noreply.github.com>
2024-04-21 19:20:34 -0300

    Update index.py

--- index.py
+++ index.py
@@ -6,7 +6,16 @@ opcao = st.sidebar.selectbox('Sobre o Streamlit',
                              ('Historia Do Streamlit', 'Funcionalidades Do Streamlit',
                               'Projeto Streamlit')
                              )
-sidebar = True
+def open_sidebar():
+    js = """
+    <script>
+    document.getElementsByClassName('sidebar-container')[0].style = 'display: block'
+    </script>
+    """
+    st.markdown(js, unsafe_allow_html=True)
+
+# Chamando a função para abrir a barra lateral
+open_sidebar()
 st.sidebar.text('Alunos:\n'
                 'Daniel\n'
                 'Henrique\n'
